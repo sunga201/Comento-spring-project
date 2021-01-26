78fec9aec9ea8c442c787b27a4b07865ec3e383b
sunga201 <sung9255@gmail.com>
2021-01-26 15:39:29 +0900

    1주차 코드 위치 변경.

--- src/main/java/com/hong/dao/MovieDAO.java
+++
@@ -1,8 +0,0 @@
-package com.hong.dao;
-
-import java.util.List;
-import com.hong.vo.MovieVO;
-
-public interface MovieDAO {
-	public List<MovieVO> selectMovie() throws Exception;
-}
--- src/main/java/com/hong/service/MovieService.java
+++
@@ -1,9 +0,0 @@
-package com.hong.service;
-
-import java.util.List;
-
-import com.hong.vo.MovieVO;
-
-public interface MovieService {
-	public List<MovieVO> selectMovie() throws Exception;
-}
