2a5bae8381ac8c67bb8029464a341f2b21cf5277
sunga201 <sung9255@gmail.com>
2021-02-03 10:23:41 +0900

    월별 접속자 수, 일자별 접속자 수, 평균 하루 로그인 수, 부서별 월별 로그인 수 API 추가.

--- springboot-practice/src/main/java/com/example/demo/dao/StatisticMapper.java
+++ springboot-practice/src/main/java/com/example/demo/dao/StatisticMapper.java
@@ -1,10 +1,13 @@
 package com.example.demo.dao;
 
 import java.util.HashMap;
+import java.util.List;
 
 public interface  StatisticMapper {
     public HashMap<String, Object> selectYearLogin(String year);
     public HashMap<String, Object> selectMonthLogin(String month);
     public HashMap<String, Object> selectDayLogin(String day);
+    public List<HashMap<String, Object>> selectAverageLogin();
+    public HashMap<String, Object> selectExceptHolidayLogin(String year, String month);
     public HashMap<String, Object> selectDepartmentMonthLogin(String dep, String month);
 }
